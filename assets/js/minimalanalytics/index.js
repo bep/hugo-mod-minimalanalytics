a59b2c50aa1a583ab866becf358538fd5e8f123b
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2025-11-04 19:10:14 +0100

    Check that navigator.sendBeacon is supported before using it

--- assets/js/minimalanalytics/index.js
+++ assets/js/minimalanalytics/index.js
@@ -3,6 +3,12 @@ import * as params from '@params';
 
 (function () {
 	'use strict';
+
+	if (!navigator.sendBeacon) {
+		// TODO(bep) remove this when https://github.com/jahilldev/minimal-analytics/pull/57 is merged.
+		console.warn('minimalanalytics: navigator.sendBeacon is not supported in this browser');
+		return;
+	}
 	track(params.tracking_id);
 	window.track = track;
 })();
